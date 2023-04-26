0941908adf711a7891a2a151792c08093065fc7f
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-04-26 11:14:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
+st.title("우버 픽업 예제") 
 chart_data = pd.DataFrame(
     np.random.randn(20, 3),
     columns=['a', 'b', 'c'])
 
 st.area_chart(chart_data)
+
 
 st.title('Uber pickups in NYC')
 
