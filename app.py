b9a2f48a2044a5ea4f74a1d88db5733d089b3f62
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-04-26 11:14:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.area_chart(chart_data)
+
 st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
